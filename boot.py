389a63b09c9367e64f44a963238bbce9514c0fa9
Win9X <59638234+windows9x95@users.noreply.github.com>
2020-02-20 21:54:15 +0100

    Create boot.py
    used in main.py, for the starting screen

---
+++ boot.py
@@ -0,0 +1,17 @@
+from time import *
+from kandinsky import *
+from ibmlogo import *
+def main():
+  fill_rect(0,0,640,480,(0,0,255))
+  draw_string(a,17,5,(255,255,255),(0,0,255))
+  draw_string(b,17,25,(255,255,255),(0,0,255))
+  draw_string(c,17,45,(255,255,255),(0,0,255))
+  draw_string(d,17,65,(255,255,255),(0,0,255))
+  draw_string(e,17,85,(255,255,255),(0,0,255))
+  draw_string(f,17,105,(255,255,255),(0,0,255))
+  draw_string(g,17,125,(255,255,255),(0,0,255))
+  draw_string(h,17,145,(255,255,255),(0,0,255))
+  draw_string(i,17,165,(255,255,255),(0,0,255))
+  draw_string(j,17,185,(255,255,255),(0,0,255))
+  draw_string(k,17,205,(255,255,255),(0,0,255))
+  sleep(5)
